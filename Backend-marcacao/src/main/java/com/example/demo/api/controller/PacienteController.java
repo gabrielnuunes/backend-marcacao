82d97adb543e0170afc28f9456fe370a5684ce75
gabriel <gabrielnuunes22@gmail.com>
2022-09-15 11:57:22 -0300

    Implementado o método PUT no controller de paciente

--- Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
@@ -2,11 +2,13 @@ package com.example.demo.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,6 +42,15 @@ public class PacienteController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Paciente adicionar(@RequestBody Paciente paciente) {
 		return pacienteRepository.save(paciente);
+	}
+	
+	@PutMapping("/{pacienteId}")
+	public Paciente atualizar(@PathVariable Long pacienteId, @RequestBody Paciente paciente) {
+		Paciente pacienteAtual = cadastroPaciente.successOrFail(pacienteId);
+		
+		BeanUtils.copyProperties(paciente, pacienteAtual, "id");
+		
+		return pacienteRepository.save(pacienteAtual);
 	}
 	
 	
