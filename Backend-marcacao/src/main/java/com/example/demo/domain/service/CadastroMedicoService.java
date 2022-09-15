961923eab95398e18200c0c46cced5e1c94efacf
gabriel <gabrielnuunes22@gmail.com>
2022-09-15 16:34:38 -0300

    Implementado a classe de Domínio/Serviço para o cadastro de médico

--- Backend-marcacao/src/main/java/com/example/demo/domain/service/CadastroMedicoService.java
+++ Backend-marcacao/src/main/java/com/example/demo/domain/service/CadastroMedicoService.java
@@ -1,8 +1,11 @@
 package com.example.demo.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.exception.EntidadeEmUsoException;
 import com.example.demo.domain.exception.MedicoNaoEncontradoException;
 import com.example.demo.domain.model.Medico;
 import com.example.demo.domain.repository.MedicoRepository;
@@ -16,8 +19,12 @@ public class CadastroMedicoService {
 	public void excluir(Long medicoId) {
 		try {
 			medicoRepository.deleteById(medicoId);
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (EmptyResultDataAccessException e) {
+			throw new MedicoNaoEncontradoException(
+					String.format("Não existe um cadastro de médico com código %d ", medicoId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Médico de código %d não pode ser removido, pois está em uso ", medicoId));
 		}
 	}
 	
