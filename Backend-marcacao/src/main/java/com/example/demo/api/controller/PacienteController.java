56f8bddc553bf215b300f22e89733e1bf94bb6a2
gabriel <gabrielnuunes22@gmail.com>
2022-09-19 08:45:28 -0300

    Implementado o tratamento de exceções no controlador com ExceptionHandler

--- Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.Paciente;
 import com.example.demo.domain.repository.PacienteRepository;
 import com.example.demo.domain.service.CadastroPacienteService;
@@ -61,5 +63,9 @@ public class PacienteController {
 	}
 	
 	
+	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+		Problem problema = new Problem();
+		
+	}
 	
 }
