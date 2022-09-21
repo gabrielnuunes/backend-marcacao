5e080d76525f6c78ec3f76e04b351cc784de90ff
gabriel <gabrielnuunes22@gmail.com>
2022-09-21 10:46:11 -0300

    Implementado o método POST no controlador de médico

--- Backend-marcacao/src/main/java/com/example/demo/api/controller/MedicoController.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/controller/MedicoController.java
@@ -3,8 +3,12 @@ package com.example.demo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.example.demo.domain.model.Medico;
 import com.example.demo.domain.repository.MedicoRepository;
@@ -28,5 +32,10 @@ public class MedicoController {
 	public Medico buscar(@PathVariable Long medicoId) {
 		return cadastroMedico.successOrFail(medicoId);
 	}
-	
+		
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Medico adicionar(@RequestBody Medico medico) {
+		return medicoRepository.save(medico);
+	}
 }
