d7116a797d7ae41ae759db1a335127940711909d
gabriel <gabrielnuunes22@gmail.com>
2022-09-27 14:21:06 -0300

    implementando o Enum de tipo de problema para tratar as exceções da classe APIException

--- Backend-marcacao/src/main/java/com/example/demo/api/exceptionhandler/ProblemType.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/exceptionhandler/ProblemType.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 public enum ProblemType {
 
 	MENSAGEM_NAO_LEGIVEL("/mensagem-nao-legivel", "Mensagem não legível"),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
+	ERRO_DE_NEGOCIO("/erro-de-negocio", "Violação de regra de negócio"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada.");
 	
 	private String title;
