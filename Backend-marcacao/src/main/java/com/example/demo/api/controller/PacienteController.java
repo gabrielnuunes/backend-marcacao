5f67a4880eedef3abc16b0e76018373c4ded75a7
gabriel <gabrielnuunes22@gmail.com>
2022-09-29 17:00:07 -0300

    Implementando as exceções, nas atualizações parciais do método Patch.

--- Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
@@ -1,22 +1,34 @@
 package com.example.demo.api.controller;
 
-import com.example.demo.api.exceptionhandler.Problem;
-import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
-import com.example.demo.domain.model.Paciente;
-import com.example.demo.domain.repository.PacienteRepository;
-import com.example.demo.domain.service.CadastroPacienteService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Field;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import com.example.demo.domain.model.Paciente;
+import com.example.demo.domain.repository.PacienteRepository;
+import com.example.demo.domain.service.CadastroPacienteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping(value = "/pacientes")
@@ -60,28 +72,42 @@ public class PacienteController {
 	}
 
 	@PatchMapping("/{pacienteId}")
-	public Paciente atualizarParcial(@PathVariable Long pacienteId, @RequestBody Map<String, Object> campos) {
+	public Paciente atualizarParcial(@PathVariable Long pacienteId, @RequestBody Map<String, Object> campos,
+			HttpServletRequest request) {
 		Paciente pacienteAtual = cadastroPaciente.successOrFail(pacienteId);
-		merge(campos, pacienteAtual);
+		
+		merge(campos, pacienteAtual, null);
 
 		return atualizar(pacienteId, pacienteAtual);
 	}
 
-	private void merge(Map<String, Object> dadosOrigem, Paciente pacienteDestino) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Paciente pacienteOrigem = objectMapper.convertValue(dadosOrigem, Paciente.class);
+	private void merge(Map<String, Object> dadosOrigem, Paciente pacienteDestino,
+			HttpServletRequest request) {
+		
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
+		
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();		
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+			
+			Paciente pacienteOrigem = objectMapper.convertValue(dadosOrigem, Paciente.class);
+			
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Paciente.class, nomePropriedade);
+				field.setAccessible(true);
+				
+				Object novoValor = ReflectionUtils.getField(field, pacienteOrigem);
+				
+				ReflectionUtils.setField(field, pacienteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+		}
+		
 
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			Field field = ReflectionUtils.findField(Paciente.class, nomePropriedade);
-			field.setAccessible(true);
-
-			Object novoValor = ReflectionUtils.getField(field, pacienteOrigem);
-
-			ReflectionUtils.setField(field, pacienteDestino, novoValor);
-
-		} );
+		} 
 
 	}
 
-
-}
