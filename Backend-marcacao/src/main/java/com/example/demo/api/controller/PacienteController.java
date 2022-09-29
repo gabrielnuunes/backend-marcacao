1dc79c890eb4ee212e22f17bbf135dc125e02a59
Gabriel <gabrielnuunes22@gmail.com>
2022-09-28 22:02:39 -0300

    Criado o método Patch no controlador de paciente, para realizar uma atualização parcial da entidade, e criado o método merge, o qual faz o merge dos atributos que se deseja atualizar parcialmente.

--- Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
+++ Backend-marcacao/src/main/java/com/example/demo/api/controller/PacienteController.java
@@ -5,14 +5,18 @@ import com.example.demo.domain.exception.EntidadeNaoEncontradaException;
 import com.example.demo.domain.model.Paciente;
 import com.example.demo.domain.repository.PacienteRepository;
 import com.example.demo.domain.service.CadastroPacienteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/pacientes")
@@ -54,5 +58,30 @@ public class PacienteController {
 	public void remover(@PathVariable Long pacienteId) {
 		cadastroPaciente.excluir(pacienteId);
 	}
+
+	@PatchMapping("/{pacienteId}")
+	public Paciente atualizarParcial(@PathVariable Long pacienteId, @RequestBody Map<String, Object> campos) {
+		Paciente pacienteAtual = cadastroPaciente.successOrFail(pacienteId);
+		merge(campos, pacienteAtual);
+
+		return atualizar(pacienteId, pacienteAtual);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Paciente pacienteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Paciente pacienteOrigem = objectMapper.convertValue(dadosOrigem, Paciente.class);
+
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Paciente.class, nomePropriedade);
+			field.setAccessible(true);
+
+			Object novoValor = ReflectionUtils.getField(field, pacienteOrigem);
+
+			ReflectionUtils.setField(field, pacienteDestino, novoValor);
+
+		} );
+
+	}
+
 
 }
