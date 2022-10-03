f858b3c0c3df401ec21fcc9b1a82bedad91db24b
gabriel <gabrielnuunes22@gmail.com>
2022-10-03 16:12:36 -0300

    Descrevendo a documentação da API e as informações na documentação

--- Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
+++ Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
@@ -1,21 +1,33 @@
 package com.example.demo.core.openAPI;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SpringFoxConfig {
 
+	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.OAS_30)
 				.select()
-				.apis(RequestHandlerSelectors.any())
-				.build();
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.api"))
+				.build()
+				.apiInfo(apiInfo());
 	}
 	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("API Gestão de pacientes")
+				.description("Documentação de API aberta para médicos e pacientes.")
+				.version("1")
+				.contact(new Contact("Gabriel Nunes", "https://github.com/gabrielnuunes", "gabrielnuunes22@gmail.com"))
+				.build();
+	}
 }
