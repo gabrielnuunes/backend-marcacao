e05c1a181ca534c5646292fda4b872f03ffd144d
gabriel <gabrielnuunes22@gmail.com>
2022-10-05 11:33:54 -0300

    Mudanças trabalho dia 05/10/2022

--- Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
+++ Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -17,11 +19,14 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SpringFoxConfig {
 
+	TypeResolver typeResolver = new TypeResolver();
+	
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30)
@@ -31,9 +36,18 @@ public class SpringFoxConfig {
 					.build()
 				.useDefaultResponseMessages(false)
 				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+				.additionalModels(null, null)
 				.apiInfo(apiInfo())
 				.tags(new Tag("Pacientes", "Controller que gerencia os pacientes"), 
 						new Tag("Médicos", "Controller que gerencia os médicos"));
+	}
+	
+	@Bean
+	public JacksonModuleRegistrar springFoxJacksonConfig() {
+		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
 	}
 	
 	private List<Response> globalGetResponseMessages() {
@@ -49,6 +63,40 @@ public class SpringFoxConfig {
 		  );
 		}
 	
+	private List<Response> globalPostPutResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+					.description("Requisição inválida pelo lado do cliente")
+					.build(),
+			    new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno no servidor")
+					.build(),				 	
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+					.description("Recurso não possui representação aceita pelo consumidor")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+					.description("Requisição recusada, pois o corpo está em um formato não suportado")
+					.build()					
+				);
+	}
+	
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+					.description("Requisição inválida pelo lado do cliente")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno do servidor")
+					.build()
+				);
+	}
+	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
 				.title("API Gestão de pacientes")
@@ -57,4 +105,5 @@ public class SpringFoxConfig {
 				.contact(new Contact("Gabriel Nunes", "https://github.com/gabrielnuunes", "gabrielnuunes22@gmail.com"))
 				.build();
 	}
+	
 }
