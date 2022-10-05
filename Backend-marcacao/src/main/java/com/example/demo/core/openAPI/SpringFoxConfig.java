3ddb82b79faf6b63a81cbf36edd6c4e75dbb5f97
Gabriel <gabrielnuunes22@gmail.com>
2022-10-04 22:52:31 -0300

    Colocando os modelos de erros e problemas com código de erro

--- Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
+++ Backend-marcacao/src/main/java/com/example/demo/core/openAPI/SpringFoxConfig.java
@@ -2,11 +2,13 @@ package com.example.demo.core.openAPI;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.example.demo.api.exceptionhandler.Problem;
 import com.fasterxml.classmate.TypeResolver;
@@ -14,8 +16,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.var;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
@@ -38,6 +42,9 @@ public class SpringFoxConfig {
 					.build()
 				.useDefaultResponseMessages(false)
 				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
 				.additionalModels(typeResolver.resolve(Problem.class))
 				.apiInfo(apiInfo())
 				.tags(new Tag("Pacientes", "Controller que gerencia os pacientes"), 
@@ -45,17 +52,63 @@ public class SpringFoxConfig {
 	}
 	
 	private List<Response> globalGetResponseMessages() {
-		  return Arrays.asList(
-		      new ResponseBuilder()
-		          .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-		          .description("Erro interno do Servidor")
-		          .build(),
-		      new ResponseBuilder()
-		          .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-		          .description("Recurso não possui representação que pode ser aceita pelo consumidor")
-		          .build()
-		  );
-		}
+	    return Arrays.asList(
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno do Servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+	                    .description("Recurso não possui representação que pode ser aceita pelo consumidor")
+	                    .build()
+	    );
+	}
+	
+	private List<Response> globalPostPutResponseMessages() {
+	    return Arrays.asList(
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+	                    .description("Requisição inválida (erro do cliente)")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno no servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+	                    .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+	                    .description("Requisição recusada porque o corpo está em um formato não suportado")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build()
+	    );
+	}
+	
+	private List<Response> globalDeleteResponseMessages() {
+	    return Arrays.asList(
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+	                    .description("Requisição inválida (erro do cliente)")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build(),
+	            new ResponseBuilder()
+	                    .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+	                    .description("Erro interno no servidor")
+	                    .representation( MediaType.APPLICATION_JSON )
+	                    .apply(getProblemaModelReference())
+	                    .build()
+	    );
+	}
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
@@ -69,6 +122,12 @@ public class SpringFoxConfig {
 	@Bean
 	public JacksonModuleRegistrar springFoxJacksonConfig() {
 		return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
+	}
+	
+	private Consumer<RepresentationBuilder> getProblemaModelReference() {
+	    return r -> r.model(m -> m.name("Problema")
+	            .referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+	                    q -> q.name("Problema").namespace("com.example.demo.api.exceptionhandler")))));
 	}
 	
 }
